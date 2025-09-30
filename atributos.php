d5f6ea48366b0dfdfc975138fcd7c73504b6a9fc
lalamacedo <lauanacamilly557@gmail.com>
2025-09-30 08:23:31 -0300

    atualizando select e insert

---
+++ atributos.php
@@ -0,0 +1,6 @@
+isset - verifica o valor
+empty - vazio, se não for vazio ativa o trim 
+trim - retira os espaços da string
+query - realiza uma consulta
+num_rows - obtem o numero de linhas que cria na consulta
+fetch_assoc - pega a linha que eu quero, se tiver dentro do while  ele pega a primeira
