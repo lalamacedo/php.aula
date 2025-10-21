c61563846cc4b08414ed853b7b080932986fad69
lalamacedo <lauanacamilly557@gmail.com>
2025-10-21 08:05:43 -0300

    subindo codigo sem erros

--- todo-list-mvc/app/config/database.php
+++ todo-list-mvc/app/config/database.php
@@ -1,23 +1,21 @@
-<?php
+<?php 
 
-class Database //toda vez que eu for criar um banco volta pra essa classe
-{
-    private  $host = "localhost";
-    private $usuario = "root";
-    private $senha = "";
-    private $banco = "to_dolist";
+class Database{
 
-    public $conn;
-    public function conectar () : mysqli {
-        $this->conn = new mysqli($this->host, $this->usuario, $this->senha, $this->banco);
-       
-       if ($this->conn->connect_error) {
-        die("Deu erro mana: ". $this->conn->connect_error);
-       }
-        return $this->conn;
-        
+    private $host = "localhost"; 
+    private $usario = "root"; 
+    private $senha = ""; 
+    private $banco = "to_dolist"; 
+    public $conn; 
+
+    public function conectar(){
+        $this->conn = new mysqli($this->host, $this->usario, $this->senha, $this->banco); 
+        if($this->conn->connect_error){
+            die ("Algo deu errado com a conexão" . $this->conn->connect_error); 
+        }
+
+        return $this->conn; 
+    }
+
 }
-}   
-
-
 ?>
---
+++ todo-list-mvc/index.php
@@ -0,0 +1,17 @@
+<?php
+require_once __DIR__ . '/app/controllers/TarefaController.php';
+
+$controller = new TarefaController(); 
+
+$action = $_GET['action'] ?? 'index' ; 
+
+switch ($action) {
+    case 'criar':
+        $controller->criar(); 
+    case 'excluir': 
+        $controller->excluir(); 
+    default: 
+        $controller->index(); 
+}
+
+?>
