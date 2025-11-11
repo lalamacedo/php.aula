b93e7da2370a130d8a04e3a6db93ca876a0e318e
lalamacedo <lauanacamilly557@gmail.com>
2025-11-11 07:50:04 -0300

    subindo novo arquivo

--- login/conexao.php
+++ login/conexao.php
@@ -1,13 +1,12 @@
 <?php
-$host = 'localhost';
-$usuario = 'root';
-$senha = '';
-$database = 'login';
+$host = 'localhost'; 
+$usuario = 'root'; 
+$senha = ''; 
+$database = 'login'; 
 
-$conn = new mysqli($host, $usuario, $senha, $database);
+$mysqli = new mysqli($host, $usuario, $senha, $database);
 
-if ($conn-> connect_error){
-    die ("Não deu certo mona");
+if($mysqli->connect_error){
+    die ("Não deu certo fazer a conexão"); 
 }
-
 ?>
